ac2d6b407ba664882bfc7995c508feeea700f602
julieveal <julieclucas@att.net>
2013-07-31 23:20:45 -0400

    changed gitrepo

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,12 @@
+ // Veal_Julie  July 31, 2013  circumference
+
+ //calculate the circumference of a circle
+
+var total =  calcCircumference(60, 3) //the function
+
+function calcCircumference(d, pi){
+	var circumference = d * pi;
+	return circumference;
+
+}
+console.log(total);
